c5cbd09b07e781c001b3b8cce9024c22a983d3ea
Titus Leistner <mail@titus-leistner.de>
2018-08-22 02:28:46 +0200

    Bump version: 0.2.1 → 0.2.2

--- homekit_mqtt/__init__.py
+++ homekit_mqtt/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Titus Leistner"""
 __email__ = 'mail@titus-leistner.de'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/titus-leistner/homekit_mqtt',
-    version='0.2.1',
+    version='0.2.2',
     zip_safe=False,
 )
