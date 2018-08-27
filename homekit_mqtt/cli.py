f3155c874455ef9082e11bc84aebbed3d1c6c9ad
Titus Leistner <mail@titus-leistner.de>
2018-08-28 01:08:00 +0200

    Add systemd setup

--- homekit_mqtt/cli.py
+++ homekit_mqtt/cli.py
@@ -35,19 +35,37 @@ def create_cfg(dname):
     shutil.copyfile(src, dst)
 
 
+def systmd():
+    """
+    Setup a systemd service for homekit-mqtt
+    """
+    src = os.path.join((os.path.abspath(os.path.dirname(__file__))),
+                       'data', 'homekit-mqtt.service')
+    dst = '/etc/systemd/system/homekit-mqtt.service'
+    os.link(src, dst)
+
+    # reload daemons
+    os.system('systemctl daemon-reload')
+
+
 @click.command()
-@click.option('--reset/--load', default=False,
-              help='Reset the bridge before readding it \
-                    to the Home App again.')
 @click.option('--cfg', default='/etc/homekit-mqtt',
               help='The directory containing the accessory configuration.')
-def main(reset, cfg):
+@click.option('--reset/--load', is_flag=True,
+              help='Reset the bridge before readding it \
+                    to the Home App again.')
+@click.option('--setup-systemd', is_flag=True,
+              help='Create a systemd service.')
+def main(cfg, reset, setup_systemd):
     # init logging
     logging.basicConfig(level=logging.INFO)
 
     if reset:
         # remove accessory.state
         os.remove('accessory.state')
+
+    if setup_systemd:
+        systmd()
 
     # create config if necessary
     if not os.path.exists(os.path.join(cfg, 'bridge.cfg')):
